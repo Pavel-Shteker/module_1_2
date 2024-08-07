840d6f26e7e7b8db8f34e270f65db645800fd8ae
Pavel-Shteker <panasg@mail.ru>
2024-08-07 03:47:51 -0700

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,8 @@
+course = 'Python'
+count_dz = 12
+wasted_time = 1.5
+
+print ('Курс:', course,
+       ', всего задач:',count_dz,
+       ', затрачено часов:',wasted_time,
+       ', среднее время выполнения ', wasted_time/count_dz, '.')
